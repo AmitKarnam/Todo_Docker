a09d3727435a118aa4ece8fe045742de54a5690d
AmitKarnam <56480746+AmitKarnam@users.noreply.github.com>
2023-03-26 10:14:21 +0530

    Create client2-entrypoint.sh

---
+++ entrypoint-files/client2-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+CONFIG_FILE=/consul/config/client.json
+
+if [ -f "$CONFIG_FILE" ]; then
+  consul agent -config-file=$CONFIG_FILE &
+else
+  consul agent -dev &
+fi
+
+# Wait for consul agent to start 
+while ! consul info; do
+  sleep 1
+done
+
+# Run the consul connect proxy sidecar for web service
+consul connect proxy -sidecar-for db &
+
+
+# Wait for consul connect proxy to start
+while ! curl -sS localhost:21001 > dev/null; do
+  sleep 1
+do
+
+# Execute any additional commands
+exec "$@"
